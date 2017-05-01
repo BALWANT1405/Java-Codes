a31adfcad012350eb130e867064421fad89a1ab0
BALWANT1405 <balwantbsb@gmail.com>
2017-05-01 11:10:03 +0530

    ADDING ARR_CONSOLE FILE

---
+++ arr_console.java
@@ -0,0 +1,21 @@
+
+public class arr_console
+{	
+	public static void main(String[] args) 
+	{
+
+	int i;
+	String[] name=new String[4];
+	name[0]="MY NAME";
+	name[1]="IS";
+	name[2]="BALWANT";
+	name[3]="SINGH";
+
+	for(i=0;i<name.length;i++)
+	{
+		System.out.println(name[i]);
+	}
+		
+	}
+
+}
