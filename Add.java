54520fb91b9523e6003b24278be2919020325be6
BALWANT1405 <balwantbsb@gmail.com>
2017-06-16 12:16:20 +0530

    Add in main method using global variable

---
+++ Add.java
@@ -0,0 +1,16 @@
+package my_core_java_codes;
+
+class Add
+{
+int a=15,c=0,d=0;
+public static void main(String[] args)
+{
+Add obj=new Add();
+if(obj.a==15)
+{
+obj.c=15;
+obj.d=20;
+}
+System.out.println(obj.c+obj.d);
+}
+}
